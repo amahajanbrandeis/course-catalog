6b2bda9d53da7f7cb226e609605a1c7f24634495
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-12 19:09:50 -0400

    User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'users/new'
+
   resources :catalog do
     collection do
       get 'search'
     end
   end
 
+ get    '/signup',  to: 'users#new'
+ get    '/login',   to: 'sessions#new'
+ post   '/login',   to: 'sessions#create'
+ delete '/logout',  to: 'sessions#destroy'
+ resources :users
+
   #get 'catalogs/search'
 
-  root 'catalogs#search'
+ root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
