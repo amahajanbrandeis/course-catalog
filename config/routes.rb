3f9daf1f5d90c0674608c7edf99acde9a4f45e86
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-14 23:59:16 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
- 
+ get 'courses/new'
+
+ get 'courses/show'
+
+ get 'courses/index'
+
+ resources :courses
+
+ get    '/courses',   to: 'courses#index'
+
+ root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
