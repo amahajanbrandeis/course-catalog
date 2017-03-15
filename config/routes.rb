aa367bc24ff6467ced7e152ff094f0b42b588a19
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-15 01:16:49 -0400

    fixed format of routes file, and added password fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'instructors/new'
-
+    get 'instructors/new'
     get 'instructors/show'
-
     get 'instructors/index'
 
     get 'courses/new'
-
     get 'courses/show'
-
     get 'courses/index'
 
     get 'subjects/new'
     get 'subjects/show'
 
     get 'sessions/new'
+
     get 'users/new'
 
     resources :catalog do
@@ -23,21 +20,17 @@ Rails.application.routes.draw do
         get 'search'
       end
     end
-
     resources :subjects
     resources :courses
     resources :instructors
     resources :sessions
+    resources :users
 
    get    '/signup',   to: 'users#new'
    get    '/login',    to: 'sessions#new'
    post   '/login',    to: 'sessions#create'
    delete '/logout',   to: 'sessions#destroy'
-   resources :users
-
-
-
- get    '/courses',   to: 'courses#index'
+   get    '/courses',   to: 'courses#index'
 
   root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
