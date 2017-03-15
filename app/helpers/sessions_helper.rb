e15496a85994efe0bbebf600ff28499ea0a77ec5
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-15 01:24:22 -0400

    added conditional to hide nav bar when not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
