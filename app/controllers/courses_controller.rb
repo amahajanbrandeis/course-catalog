7d8726bd2cadf29974614c6545a3da81f5fc5256
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-19 19:13:33 -0400

    added notification of adding course, or already enrolling in course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,12 +10,26 @@ class CoursesController < ApplicationController
   end
 
   def enroll
-    @enrollment = Enrollment.new
-    @enrollment.user_id = params[:user_id]
-    @enrollment.course_id = params[:course_id]
-    @enrollment.enrollment_date = Time.now
-    @enrollment.save
-    puts params[:course_id]
+    if !is_enrolled
+      @enrollment = Enrollment.new
+      @enrollment.user_id = params[:user_id]
+      @enrollment.course_id = params[:course_id]
+      @enrollment.enrollment_date = Time.now
+      if @enrollment.save
+        flash[:notice] = "Enrollment Successful!"
+        redirect_to enrollments_search_path
+      end
+    else
+      flash[:notice] = "Already Enrolled!"
+      redirect_to enrollments_search_path
+    end
+  end
+
+  def is_enrolled
+    user_id = params[:user_id]
+    course_id = params[:course_id]
+    enrolledCourses = Course.joins(:enrollments).where("enrollments.user_id = ? AND enrollments.course_id = ?", "#{user_id}", "#{course_id}")
+    return enrolledCourses.length > 0
   end
 
 end
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -21,46 +21,6 @@ class EnrollmentsController < ApplicationController
   def edit
   end
 
-  # POST /enrollments
-  # POST /enrollments.json
-  def create
-    @enrollment = Enrollment.new(enrollment_params)
-
-    respond_to do |format|
-      if @enrollment.save
-        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully created.' }
-        format.json { render :show, status: :created, location: @enrollment }
-      else
-        format.html { render :new }
-        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /enrollments/1
-  # PATCH/PUT /enrollments/1.json
-  def update
-    respond_to do |format|
-      if @enrollment.update(enrollment_params)
-        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @enrollment }
-      else
-        format.html { render :edit }
-        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /enrollments/1
-  # DELETE /enrollments/1.json
-  def destroy
-    @enrollment.destroy
-    respond_to do |format|
-      format.html { redirect_to enrollments_url, notice: 'Enrollment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   def search
     p = params
     enrollment = p[:enrollment]
