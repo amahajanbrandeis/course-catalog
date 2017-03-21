1c4a7c19c1882f7594e0650c89036ece31bde3fd
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-20 22:56:41 -0400

    Added destroy for enrollments, working on case sensitivity on posgres search

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -27,10 +27,19 @@ class EnrollmentsController < ApplicationController
     subject_id = enrollment[:subject_id]
     course = enrollment[:course]
     if subject_id.empty?
-      @courses = Course.joins(:subjects).where("courses.name LIKE ?", "%#{course}%")
+      #@courses = Course.joins(:subjects).where("courses.name LIKE ?", "%#{course}%")
+      t = Course.arel_table
+      @courses = Course.joins(:subjects).where(t[:name].matches("%#{course}%"))
     else
-      @courses = Course.joins(:subjects).where("courses.name LIKE ? AND subjects.subject_id = ?", "%#{course}%", "#{subject_id}")
+      @courses = Course.joins(:subjects).where("courses.name ILIKE ? AND subjects.subject_id = ?", "%#{course}%", "#{subject_id}")
+      #@courses = Course.joins(:subjects).where(courses[:name].matches(?) AND subjects.subject_id = ?", "%#{course}%", "#{subject_id}")
     end
+  end
+
+  def destroy
+    puts params
+    Enrollment.where("user_id = ? AND course_id = ?", "#{params[:user_id]}", "#{params[:course_id]}").destroy_all
+    redirect_to root_url
   end
 
   private
